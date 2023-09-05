7d66a3ac91b5c52a5761ce737f0ec6f69618e9e4
Ariel Jalkiver <arieljalkiver@gmail.com>
2023-09-05 10:27:23 +0300

    Add UI Lock

---
+++ Assets/Scripts/UserInterface/UILockManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+public class UILockManager
+{
+    private readonly Dictionary<string, bool> elementLocks = new();
+
+    // Use this method to lock/unlock a UI element
+    public void SetElementLock(string elementName, bool isLocked)
+    {
+        if (elementLocks.ContainsKey(elementName))
+        {
+            elementLocks[elementName] = isLocked;
+        }
+        else
+        {
+            elementLocks.Add(elementName, isLocked);
+        }
+    }
+
+    // Use this method to check if a UI element is locked
+    public bool IsElementLocked(string elementName)
+    {
+        if (elementLocks.ContainsKey(elementName))
+        {
+            return elementLocks[elementName];
+        }
+        // If the element is not in the dictionary, consider it as unlocked by default.
+        return false;
+    }
+
+    public void UnlockAll()
+    {
+        foreach (string elementName in elementLocks.Keys)
+        { elementLocks[elementName] = false; }
+    }
+
+    
+}
--- Assets/Scripts/UserInterface/UserInterfaceObject.cs
+++ Assets/Scripts/UserInterface/UserInterfaceObject.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -5,15 +6,20 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
+//TODO: Link properly the engineManager through api
 public class UserInterfaceObject : MonoBehaviour
 {
     private TextMeshProUGUI textBox;
-    private Button UpdateButton;
-    private Button UndoButton;
-    private Button ActionButton;
-    private GameObject canvas;
+    private Button updateButton;
+    private Button undoButton;
+    private Button extraButton;
+    private Button actionButton;
+    private Transform canvas;
+    private Transform topPanel;
     private GameEngineManager gameEngineManager;
     private Image[] dice;
+    private UILockManager lockManager = new UILockManager();
+
 
     // Start is called before the first frame update
     void Start()
@@ -35,35 +41,39 @@ public class UserInterfaceObject : MonoBehaviour
     public void Initialize(GameEngineManager manager)
     {
         gameEngineManager = manager;
-        canvas = transform.Find("Canvas").gameObject;
+        canvas = transform.Find("Canvas");
+        topPanel = canvas.transform.Find("TopPanel");
         textBox = canvas.transform.Find("TextBox").GetComponent<TextMeshProUGUI>();
-        UpdateButton = canvas.transform.Find("UpdateButton").GetComponent<Button>();
-        ActionButton = canvas.transform.Find("ActionButton").GetComponent<Button>();
-        UndoButton = canvas.transform.Find("UndoButton").GetComponent<Button>();
-        UpdateButton.onClick.AddListener(MoveTestUI);
-        UndoButton.onClick.AddListener(UndoUI);
+        updateButton = topPanel.transform.Find("UpdateButton").GetComponent<Button>();
+        undoButton = topPanel.transform.Find("UndoButton").GetComponent<Button>();
+        extraButton = topPanel.transform.Find("ExtraButton").GetComponent<Button>();
+        actionButton = canvas.transform.Find("ActionButton").GetComponent<Button>();
+        updateButton.onClick.AddListener(UpdateUI);
+        undoButton.onClick.AddListener(UndoUI);
+        extraButton.onClick.AddListener(ExtraTest);
 
         Transform panel = canvas.transform.Find("PlayerHandPanel");
 
         dice = new Image[3];
-        
-        for(int i = 0; i < 3; i++)
+
+        for (int i = 0; i < 3; i++)
         {
-            dice[i]= panel.transform.Find("Die" + (i + 1)).Find("Image").GetComponent<Image>();
+            dice[i] = panel.transform.Find("Die" + (i + 1)).Find("Image").GetComponent<Image>();
         }
 
         for (int i = 0; i < dice.Length; i++)
         {
-            dice[i].sprite = Resources.Load<Sprite>("Images/DieFaces/" + (i+1));
+            dice[i].sprite = GetDieImage(i + 1);
 
         }
 
         Debug.Log(dice.Length);
     }
 
-    private void MoveTestUI()
+    private void UpdateUI()
     {
-        gameEngineManager.TestMove();
+        if (!lockManager.IsElementLocked("UpdateButton"))
+            gameEngineManager.TestMove();
     }
 
     private void UndoUI()
@@ -78,6 +88,8 @@ public class UserInterfaceObject : MonoBehaviour
 
     public void MouseClicked(bool[] mouseButtons, Vector3 mousePos)
     {
+        if (lockManager.IsElementLocked("Map"))
+            return;
         if (mouseButtons[0] || mouseButtons[1])
         {
             gameEngineManager.SelectObject(mousePos, mouseButtons);
@@ -99,5 +111,21 @@ public class UserInterfaceObject : MonoBehaviour
     public void MouseScrolled(float deltaY)
     {
         gameEngineManager.ZoomCamera(deltaY);
+    }
+
+    private void ExtraTest()
+    {
+        lockManager.SetElementLock("UpdateButton", !lockManager.IsElementLocked("UpdateButton"));
+        lockManager.SetElementLock("Map", !lockManager.IsElementLocked("Map"));
+    }
+
+    private Sprite GetDieImage(int face)
+    {
+        if (face < 1 || face > 6)
+        {
+            Debug.LogError("Face not valid");
+            return null;
+        }
+        return Resources.Load<Sprite>("Images/DieFaces/" + (face + 1));
     }
 }
